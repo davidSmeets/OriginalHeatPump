1d48c1d11009597fbe6f7a5d1fdc0b5571525efa
davidSmeets <david-smeets@hotmail.nl>
2022-11-25 16:12:10 +0100

    Corrected labels

--- inputs.py
+++ inputs.py
@@ -3,23 +3,23 @@ import pandas as pd
 ## General tab
 
 # Type of sink ('Water', 'Air', 'Advanced')
-sink_type = 'Air'
+sink_type = "Water"
 
 # Type of source ('Water', 'Air', 'Advanced')
-source_type = 'Air'
+source_type = "Air"
 
-#temperature of sink (Celcius) in case of sinkType water: also water return temperature
+# Temperature of sink (Celcius) in case of sinkType water: also water return temperature
 sink_temp = 35
 sink_temp_return = 25
 
-#temperature of source (Celsius)
+# Temperature of source (Celsius)
 source_temp = 7
 
-#refrigerant ('r1233zd'	'r1234yf'	'r134a'	'r23'	'r236fa'	'r245fa'	'r290'	'r32'	'r404a'	'r407a'	'r407c'	'r410a'	'r438a'	'r449a'	'r452a'	'r455a'	'r507a'	'r508b'	'r513a'	'r600a'	'r717'	'r744')
-refrigerant = 'r32'
+# Refrigerant ('r1233zd'	'r1234yf'	'r134a'	'r23'	'r236fa'	'r245fa'	'r290'	'r32'	'r404a'	'r407a'	'r407c'	'r410a'	'r438a'	'r449a'	'r452a'	'r455a'	'r507a'	'r508b'	'r513a'	'r600a'	'r717'	'r744')
+refrigerant = "r290"
 
-#capacity (kiloWatt), only to be used for SCOP calculation
-capacity  = 10
+# Capacity (kiloWatt), only to be used for SCOP calculation
+capacity = 10
 
 
 ## Advanced tab
@@ -34,31 +34,35 @@ delta_T_sink_condensor = 5
 delta_T_superheat_suction = 5
 
 # Type of compressor ('Unknown', 'Screw', 'Scroll', 'Reciprocating', 'Centrifugal')
-compressor_type = 'Scroll'
+compressor_type = "Scroll"
 
 # Isentropic efficiency ('Estimate', 'Value', 'Do not include in calculation')
-isentropic_efficiency = 'Estimate'
-isentropic_efficiency_val = 65            #(%) in case of 'Value'
+isentropic_efficiency = "Estimate"
+isentropic_efficiency_val = 65  # (%) in case of 'Value'
 
 # Ideal pressure ratio (-), default value 3
-ideal_pressure_ratio = 3 
+ideal_pressure_ratio = 3
 
 # Motor efficiency
-motor_efficiency = 85 #(%)
+motor_efficiency = 85  # (%)
 
 
 ## SCOP tab
 
 # Climate profile ('Average', 'Warmer', 'Colder')
-climate_profile = 'Average'
+climate_profile = "Average"
 
 # Detailed source type ('Air/other', 'Ground (closed, vertical)', 'Ground (closed, horizontal)', 'Groundwater', 'Surface water - Lake', 'Surface water - River', 'Surface water - Sea')
-detailed_source_type = 'Air/other'
+detailed_source_type = "Air/other"
 
-inputtable = pd.DataFrame([['Sink', sink_type], 
-                            ['Source', source_type],
-                            ['Sink temperature', sink_temp],
-                            ['Sink temperature (return)', sink_temp_return],
-                            ['Source temperature', source_temp],
-                            ['Refrigerant', refrigerant],
-                            ['Capacity', capacity]])
+inputtable = pd.DataFrame(
+    [
+        ["Sink", sink_type],
+        ["Source", source_type],
+        ["Sink temperature (°C)", sink_temp],
+        ["Sink temperature (return) (°C)", sink_temp_return],
+        ["Source temperature (°C)", source_temp],
+        ["Refrigerant", refrigerant],
+        ["Capacity (kW)", capacity],
+    ]
+)
