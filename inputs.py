37427ecf0f638aa08a353102fff0a79c5e5e3684
davidSmeets <david-smeets@hotmail.nl>
2022-12-02 16:09:10 +0100

    fixed typos

--- inputs.py
+++ inputs.py
@@ -9,11 +9,11 @@ sink_type = "Water"
 source_type = "Air"
 
 # Temperature of sink (Celcius) in case of sinkType water: also water return temperature, default 10K (EN-14511 specifies only supply temp)
-sink_temp = 25
-sink_temp_return = 20
+sink_temp = 35
+sink_temp_return = 25
 
 # Temperature of source (Celsius)
-source_temp = 5
+source_temp = 7
 
 # Refrigerant ('r1233zd'	'r1234yf'	'r134a'	'r23'	'r236fa'	'r245fa'	'r290'	'r32'	'r404a'	'r407a'	'r407c'	'r410a'	'r438a'	'r449a'	'r452a'	'r455a'	'r507a'	'r508b'	'r513a'	'r600a'	'r717'	'r744')
 refrigerant = "r134a"
